9cc21ae27c6b53caf54e8114aac27b2b4547f29d
nikitagagua <2gagua121@gmail.com>
2024-02-19 20:28:26 +0200

    Initial commit

---
+++ src/main/java/DayPlanner/DayPlannerApplication.java
@@ -0,0 +1,13 @@
+package DayPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayPlannerApplication.class, args);
+	}
+
+}
